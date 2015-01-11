ad937ddeaf3cc6c990a1d39106966eb674d4128d
bricea27 <bricea27@gmail.com>
2015-01-11 16:36:04 -0500

    reworking methods / global variables

--- game.rb
+++ game.rb
@@ -3,23 +3,144 @@
 
 require "colorize"
 
-pikachu = {attack1: "", attack2: "", attack3: "", health: 100}
+#Global Variables
+pika_hp = 10
+bonus_hp = 0
+enemy_hp = 10
+still_alive = true
+enemy_dead = false
 
-puts "Hello there, please enter a name"
+#player attack method, uses enemy health as parameter
+def pika_attack(enemy_hp)
+  #randomization
+  random = rand(10) + 1 #includes 10
+  if random == 1 || random == 2
+    puts "Oh no, Pikachu missed!"
+  elsif random == 3 || random == 4 || random == 5
+    puts "Pikachu used Tail Whip."
+    enemy_hp = enemy_hp - 2
+    puts "Enemy's hp is reduced to #{enemy_hp}."
+  elsif random == 6 || random || 7 || random == 8
+    puts "Pikachu used ThunderShock."
+    enemy_hp = enemy_hp - 3
+    puts "Enemy's hp is reduced to #{enemy_hp}."
+  else
+    puts "Pikachu used Slam."
+    enemy_hp = enemy_hp - 4
+    puts "Enemy's hp is reduced to #{enemy_hp}."
+  end
+  return enemy_hp
+end #end pika_attack
+
+#enemy attack method, uses player health as parameter
+def enemy_attack(pika_hp)
+  random = rand(10) + 1 #includes 10
+  if random == 1 || random == 2 || random == 3 || random == 4
+    puts "The enemy's attack missed! Pikachu's hp is #{pika_hp}."
+  elsif random == 5 || random == 6
+    pika_hp = pika_hp - 1
+    puts "The enemy used Growl. Pikachu's hp is reduced to #{pika_hp}."
+  elsif random == 7 || random == 8
+    pika_hp = pika_hp - 2
+    puts "The enemy used Slam. Pikachu's hp is reduced to #{pika_hp}."
+  else
+    pika_hp = pika_hp - 3
+    puts "The enemy used Punch. Pikachu's hp is reduced to #{pika_hp}."
+  end
+  return pika_hp
+end #end enemy_attack
+
+def battle_sequence(enemy_hp, pika_hp, bonus_hp, still_alive)
+  enemy_dead = false
+  if enemy_hp <= 0
+    puts "You Win!"
+    pika_hp = 10 #rest pika's health
+    bonus_hp = bonus_hp + 1 #increment bonus health
+    pika_hp = pika_hp + bonus_hp #add bonus health
+    enemy_hp = 10 #restore enemy's health
+    puts "Pikachu's hp is now #{pika_hp}!"
+    puts "A new wild Pokemon has appeared! Do you wish to fight? (y/n)"
+    enemy_dead = true
+  end
+  if pika_hp <= 0
+    puts "You Lose!"
+    still_alive = false
+  end
+  return still_alive, enemy_hp, pika_hp, bonus_hp, enemy_dead
+end #end battle_sequence
+
+
+def run_game()
+  pika_attack()
+end #run_game
+
+
+
+puts "Hello there, please enter a name:".colorize(:yellow)
 player_name = gets.chomp
 
-puts "Professor Oak: 'Hello #{player_name}. Do you and Pikachu wish to catch them all? (y/n)'"
+puts "Professor Oak: 'Hello #{player_name}. Welcome to the world of Pokemon!'".colorize(:yellow)
 
-if gets.chomp.downcase == "y"
+while still_alive == true do
 
-  puts "A wild Zubat has appeared! Do you wish to fight? (y/n)"
+  puts "A wild Pokemon has appeared! Do you wish to fight? (y/n)".colorize(:red)
+  answer = gets.chomp.upcase
 
-  if gets.chomp.downcase == "y"
-    
+  if answer == "Y"
+
+    #sets enemy's health equal to pikachu's attack
+    enemy_hp = pika_attack(enemy_hp)
+
+    still_alive, enemy_hp, pika_hp, enemy_dead, bonus_hp` = battle_sequence(enemy_hp, pika_hp, bonus_hp, still_alive)
+
+    if enemy_dead == false
+      pika_hp = enemy_attack(pika_hp)
+      still_alive, enemy_hp, pika_hp, enemy_dead, bonus_hp =  battle_sequence(enemt_hp, pika_hp, bonus_hp, still_alive)
+    end
+
   else
-    puts "You are a goober"
+    puts "You lack the heart to be a true Pokemon Master".colorize(:yellow)
+    exit
   end
 
-else
-  exit
-end
+end #end while still_alive
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# #restore player health when enemy dies
+# if enemy_health == 0
+#   player_health = 10
+# end
+#
+# #end game when player dies
+# if player_health == 0
+#   is_alive = false
+#   exit
+# end
+
+
+
+# #play game as long as player is alive
+# while still_alive == true
+#   puts "Uh oh, a wild Pokemon has appeared! Do you wish to fight? (y/n)"
+#   answer = gets.chomp.upcase
+#
+#   if answer = "Y"
+#     pika_attack(enemy_hp)
+#   else
+#     pika_hp = pika_hp - 2
+#   end
+#
+#
+# end
--- game2.rb
+++ game2.rb
@@ -1,25 +1,89 @@
-# Team 4 the Win
-# GOTTA CATCH EM ALL
+puts "Hey, beastmaster. What do they call you?"
+name = gets.chomp
+health = 10
+bonus_health = 0
+monster_hp = 10
+puts "Hey, #{name}. Welcome, you have #{health} health"
+puts "A Wild baddie appears!"
+stillplaying = true
+did_it_die = false
 
-require "colorize"
+#handle probability of attack and probablity of damage (2-4)
+#set number to attack names (3 - growl - does 2 damage)
 
-pikachu = {attack1: "", attack2: "", attack3: "", health: 100}
-
-puts "Hello there, please enter a name"
-player_name = gets.chomp
-
-puts "Professor Oak: 'Hello #{player_name}. Do you and Pikachu wish to catch them all? (y/n)'"
-
-if gets.chomp.downcase == "y"
-
-  puts "A wild Zubat has appeared! Do you wish to fight? (y/n)"
-
-  if gets.chomp.downcase == "y"
-    
-  else
-    puts "You are a goober"
+def heroattack(monster_hp)
+  rnd = rand(10)+1
+  case rnd
+  when 1,2
+    puts "You attack missed..."
+    puts "-Moster's health is still #{monster_hp} "
+  when 2,3,4,5,6,7,8,9,10
+    damage_to_monster = rand(4)+1
+    puts "Your attack connects for #{damage_to_monster} damage"
+    monster_hp = (monster_hp - damage_to_monster)
+    puts "-Moster's health is reduced to #{monster_hp} "
   end
+  return monster_hp
+end
 
-else
-  exit
+def baddieattack(health)
+  rnd = rand(10)+1
+  case rnd
+  when 1,2,3,4
+    puts "Baddie's attack missed..."
+    puts "-Your health is still #{health} "           
+  when 5,6,7,8,9,10
+    damage_to_hero = rand(3)+1
+    puts "Baddie's attack connects for #{damage_to_hero} damage"
+    health = (health - damage_to_hero)
+    puts "-Your health is reduced to #{health} "
+  end
+  return health
+end
+
+def checkforwinner(monster_hp, health, bonus_health, stillplaying)
+  did_it_die = false
+  if monster_hp <= 0
+    "You win"
+    health = 10
+    bonus_health += 1
+    health = health + bonus_health
+    monster_hp = 10
+    puts "Your new health total is: #{health}"
+    puts "A NEW Wild baddie appears with #{monster_hp} hp. Fight or Run?"
+    did_it_die = true
+  end
+  if health <= 0
+    puts "You lose"
+    stillplaying = false
+  end
+  return stillplaying, monster_hp, health, did_it_die, bonus_health
+end
+
+#while loop is the actual game
+#call the methods set up before within the while loop
+while stillplaying == true do
+  puts "-------------------------------------------"
+  puts "Fight or Run"
+  answer = gets.chomp.upcase
+
+  #Same as a conditional (if answer == "RUN"...)
+  case answer
+  when "RUN", "R"
+    puts "You ran"
+    health -= 2
+    puts "-Your heath has been reduced to #{health}"
+
+  when "FIGHT", "F"
+    puts "entering battle round..."
+
+    monster_hp = heroattack(monster_hp)
+
+    stillplaying, monster_hp, health, did_it_die, bonus_health = checkforwinner(monster_hp, health, bonus_health, stillplaying)
+    if did_it_die == false then
+      health = baddieattack(health)
+      stillplaying, monster_hp, health, did_it_die, bonus_health =  checkforwinner(monster_hp, health, bonus_health, stillplaying)
+    end
+
+  end
 end
